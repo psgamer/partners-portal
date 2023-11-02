7098a9d99efbef23d2826b1baab6dd2ea6938e09
andrey.tkachov <andrey.tkachov@genixsolutions.biz>
2023-11-02 15:41:40 +0200

    PPP-24: refactoring

--- src/environments/environment.types.d.ts
+++ src/environments/environment.types.d.ts
@@ -1,9 +1,8 @@
 import {FirebaseConfig} from '../firebase.config';
 
-interface Config {
+export type EnvironmentConfig = Readonly<{
   production: boolean,
   useEmulators: boolean,
   defaultauth: 'firebase', // TODO dispose of this, always firebase
-  firebase: Readonly<FirebaseConfig>,
-}
-export type EnvironmentConfig = Readonly<Config>;
+  firebase: FirebaseConfig,
+}>;
